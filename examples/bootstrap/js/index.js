287b66bf130936b3dcbb26d889e0cb49fcd51d26
Paul Taylor <me@ptylr.com>
2020-04-08 13:29:02 +0100

    Refactored routing capability.

--- examples/bootstrap/js/index.js
+++ examples/bootstrap/js/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Switch } from "react-router";
-import { HashRouter as Router } from "react-router-dom";
-import { renderRoutes } from "react-router-config";
-import routeData from '../routes.json';
-import componentRegistry from "./routing";
+import Routing from "./routing";
+
+Routing.processRoutes();
 
 //TODO: import { DXM_USERNAME } from 'react-native-dotenv';
-
-const routes = [];
-Object.keys(routeData).forEach(function(key) {
-    const route = {
-        "path": routeData[key].path,
-        "exact": (routeData[key].exact === true),
-        "component": componentRegistry[routeData[key].component]
-    };
-    routes.push(route);
-});
-
-ReactDOM.render(
-    <Router>
-        <Switch>
-            {renderRoutes(routes)}
-        </Switch>
-    </Router>,
-    document.getElementById('app')
-);
--- examples/bootstrap/js/routing.js
+++ examples/bootstrap/js/routing.js
@@ -1,7 +1,38 @@
 import BlogPage from "../pages/blogpage";
+import ReactDOM from "react-dom";
+import {HashRouter as Router} from "react-router-dom";
+import {Switch} from "react-router";
+import {renderRoutes} from "react-router-config";
+import React from "react";
 
-const componentRegistry = {
-    "BlogPage": BlogPage
+export default class Routing {
+    static processRoutes()
+    {
+        const componentRegistry = {
+            "BlogPage": BlogPage
+        };
+
+        const routes = [];
+        fetch('/routes.json')
+            .then(res => res.json())
+            .then((routeData) => {
+                Object.keys(routeData).forEach(function(key) {
+                    const route = {
+                        "path": routeData[key].path,
+                        "exact": (routeData[key].exact === true),
+                        "component": componentRegistry[routeData[key].component]
+                    };
+                    routes.push(route);
+                });
+                ReactDOM.render(
+                    <Router>
+                        <Switch>
+                            {renderRoutes(routes)}
+                        </Switch>
+                    </Router>,
+                    document.getElementById('app')
+                );
+            })
+            .catch(console.log);
+    }
 }
-
-export default componentRegistry;
