749bcc125bc69f592ddfd0a56bf4958d2553b6bb
Richard Lund <richard.lund@crownpeak.com>
2020-04-20 15:14:28 +0100

    Change date to a proper Solr example using facets.

--- examples/bootstrap/components/postArchives.js
+++ examples/bootstrap/components/postArchives.js
@@ -7,8 +7,8 @@ export default class PostArchives extends CmsComponent
     constructor(props)
     {
         super (props);
-        const data = CmsDynamicDataProvider.getDynamicQuery("q=*:*&rows=50&fq=custom_s_type:'Blog%20Page'&fl=custom_i_asset_id,title,custom_dt_created");
-        this.months = data.response.docs.map((d,i) => new Date(d.custom_dt_created).toLocaleDateString());
+        const data = CmsDynamicDataProvider.getDynamicQuery("q=*:*&fq=custom_s_type:\"Blog%20Page\"&rows=0&facet=true&facet.mincount=1&facet.range=custom_dt_created&f.custom_dt_created.facet.range.start=NOW/YEAR-1YEAR&f.custom_dt_created.facet.range.end=NOW/YEAR%2B1YEAR&f.custom_dt_created.facet.range.gap=%2B1MONTH");
+        this.months = data.facet_counts.facet_ranges.custom_dt_created.counts.filter((_c, i) => i%2 === 0);
     }
 
     render() {
@@ -17,7 +17,7 @@ export default class PostArchives extends CmsComponent
                 <h4 className="font-italic">Archives</h4>
                 <ol className="list-unstyled mb-0">
                     {this.months.map((month) => {
-                        return <li key={month.toString()}><Link to={`/posts/months/${month.toString()}`}>{ month }</Link></li>
+                        return <li key={month.substr(0,7)}><Link to={`/posts/months/${month.substr(0,7)}`}>{ [new Date(month).toLocaleString('default', { month: 'long', year: 'numeric' })] }</Link></li>
                     })}
                 </ol>
             </div>
