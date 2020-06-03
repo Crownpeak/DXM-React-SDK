44a9984f1a1b142439307284278d318b3513a13b
Paul Taylor <me@ptylr.com>
2020-06-03 11:07:40 +0100

    Updated to include CmsField override value.

--- examples/bootstrap-homepage/components/heroContainer.js
+++ examples/bootstrap-homepage/components/heroContainer.js
@@ -7,16 +7,9 @@ export default class HeroContainer extends CmsComponent
     constructor(props)
     {
         super(props);
-        this.heading = new CmsField("Heading", CmsFieldTypes.TEXT);
-        this.description = new CmsField("Description", CmsFieldTypes.WYSIWYG);
-        this.button_text = new CmsField("Button_Text", CmsFieldTypes.TEXT);
-
-        if(props.data)
-        {
-            this.heading = props.data.Heading;
-            this.description = props.data.Description;
-            this.button_text = props.data.Button_Text;
-        }
+        this.heading = new CmsField("Heading", CmsFieldTypes.TEXT, props && props.data ? props.data.Heading : null);
+        this.description = new CmsField("Description", CmsFieldTypes.WYSIWYG, props && props.data ? props.data.Description : null);
+        this.button_text = new CmsField("Button_Text", CmsFieldTypes.TEXT, props && props.data ? props.data.Button_Text : null);
     }
 
     render () {
--- sdk/README.md
+++ sdk/README.md
@@ -49,3 +49,4 @@ Data Sources**
 | 1.0.8         | 2020MAY12     | cmsify.js now called from Node /.bin.    |
 | 1.0.10         | 2020MAY27     | Added CSS parsing/upload support. Added Wrapper selection.    |
 | 1.0.11         | 2020JUN02     | Added DropZone support for DXM. Added command line parameters. Bug fixes.    |
+| 1.0.12         | 2020JUN03     | Updated crownpeak-dxm-sdk-core version.    |
