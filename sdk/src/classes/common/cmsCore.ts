ec4ea5ba283fa0e4674097ad932e7714b650a129
Richard Lund <richard.lund@crownpeak.com>
2021-01-11 16:28:52 +0000

    CmsCore change to help with TypeScript for DropZone.

--- sdk/src/classes/common/cmsCore.ts
+++ sdk/src/classes/common/cmsCore.ts
@@ -1,7 +1,8 @@
 import { CmsDataCache, ICmsDataProvider, CmsNullDataProvider } from 'crownpeak-dxm-sdk-core';
 import * as React from 'react';
+import IDropZoneProps from '../components/IDropZoneProps';
 
-export default class CmsCore extends React.Component {
+export default class CmsCore extends React.Component<IDropZoneProps> {
     cmsDataProvider: ICmsDataProvider = new CmsNullDataProvider();
     cmsAssetId: number = -1;
 
